d715761bc5fd0ed7d48609a9a4fac94738f8e230
Jesper T. Rasmussen <jthabo@gmail.com>
2020-11-11 11:49:45 +0100

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,26 @@
+var movies = {
+	"Willow": {
+		"Name": "Willow"
+		"YouTubeID": "v=uzn2izehkno"
+	},
+	"UP": {
+		"Title": "Up",
+		"YouTubeId": "ORFWdXl_zJ4"
+	},
+  	 "Lord of the Rings": {
+		"Title": "Lord of the Rings",
+		"YouTubeID": "V75dMMIW2B4 "
+	},
+  	"The Princess Bride": {
+  		"Title": "The princess bride",
+        		"YouTubeId": "WNNUcHRiPS8"
+	},
+	"Alice in Wonderland": {
+		"Title": "Alice in Wonderland",
+		"YouTubeId": "P_IVeP3i7B0"
+	},
+	"Indiana Jones": {
+		"Name": "Indiana Jones"
+		"YouTubeID": "v=Rh_BJXG1-44"
+	},
+};
